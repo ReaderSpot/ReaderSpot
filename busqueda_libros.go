d05995771ce9de6be28d0bc5bf5c95b60151558f
Nestor Gavilanes <negavilaneson@uide.edu.ec>
2025-06-15 21:54:11 -0500

    Delete busqueda_libros.go
    Cambio de archivos

--- busqueda_libros.go
+++
@@ -1,8 +0,0 @@
-// Permite realizar búsquedas por diferentes criterios (título, autor, género), tambien por estado de lectura (no leido, leyendo, leido).
-package main
-
-import "fmt"
-
-func busqueda_libros_func() {
-	fmt.Print("Modulo busqueda libros")
-}
