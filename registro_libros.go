bbe3e38a685b836688ddd499fcc64faa6c5c1bed
Nestor Gavilanes <negavilaneson@uide.edu.ec>
2025-06-15 21:54:36 -0500

    Delete registro_libros.go
    Cambio de archivos

--- registro_libros.go
+++
@@ -1,8 +0,0 @@
-// Permite agregar nuevos libros electrónicos con información como título, autor, año, género y archivo digital.
-package main
-
-import "fmt"
-
-func registro_libros_func() {
-	fmt.Print("Modulo registro_libros")
-}
