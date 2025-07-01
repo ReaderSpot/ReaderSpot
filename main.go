7b725c71c8924f663a022523b253901a22c4adb9
nestorgonate <negavilaneson@uide.edu.ec>
2025-07-01 08:29:53 -0500

    Cambios en controllers, archivos

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
+/*
+Autor:
+Tarea:
+Fecha:
+*/
 package main
 
 import (
-	"ReaderSpot/controllers"
-	"ReaderSpot/db"
-	"ReaderSpot/middlewares"
-	"ReaderSpot/models"
-	"time"
+	"sistema_de_gestion_de_libros_electronicos/controllers"
+	"sistema_de_gestion_de_libros_electronicos/db"
+	"sistema_de_gestion_de_libros_electronicos/middlewares"
+	"sistema_de_gestion_de_libros_electronicos/models"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -16,64 +19,44 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	err = db.AutoMigrate(models.Libro{}, models.Categoria{}, models.Usuario{})
+	err = db.AutoMigrate(models.Libro{}, models.Usuario{}, models.Transaccion{})
 	if err != nil {
 		panic(err)
 	}
-	/*	|------------------------------|
-		|Crea la configuracion del CORS|
-		|------------------------------|
-	*/
-	router := gin.Default()
-	router.Use(cors.New(cors.Config{
-		//direccion de solicitudes permitidas
-		AllowOrigins: []string{"http://readerspot.xyz"},
-		/*	____________________________________
-			|Metodos permitidos a la direccion |
-			|__________________________________|
-		*/
-		AllowMethods: []string{"PUT", "PATCH", "GET", "POST"},
-		/*	____________________________________
-			|Permite Headers personalizados	   |
-			|__________________________________|
-		*/
-		AllowHeaders: []string{"Origin"},
-		/*	_____________________________________________________
-			|Permite exponer directamente los Headers al usuario |
-			|____________________________________________________|
-		*/
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		/*	______________________________________
-			|Cache de cada solicitud echa a la API|
-			|_____________________________________|
-		*/
-		MaxAge: 6 * time.Hour,
-	}))
-
-	/*
-		-------------------
-		endpoint de prueba
-		-------------------
-	*/
-	router.LoadHTMLGlob("views/*")
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(200, "index.html", nil)
+	r := gin.Default()
+	r.LoadHTMLGlob("views/*.html")
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(200, "index.html", nil)
 	})
-	router.GET("/registro", func(c *gin.Context) {
-		c.HTML(200, "registro.html", nil)
+	r.GET("/registro", func(ctx *gin.Context) {
+		ctx.HTML(200, "registro.html", nil)
 	})
-	router.POST("/registro", controllers.RegistrarUsuario)
-	router.GET("/login", func(c *gin.Context) {
-		c.HTML(200, "login.html", nil)
+	r.POST("/registro", controllers.Registro)
+	r.GET("/login", func(ctx *gin.Context) {
+		ctx.HTML(200, "iniciar_sesion.html", nil)
 	})
-	router.POST("/login", controllers.LoginUsuario)
-	validate := router.Group("/valid")
-	validate.Use(middlewares.AuthMiddleware())
+	r.POST("/login", controllers.Iniciar_sesion)
+	r.Static("/static", "./static")
+	//Endpoints a los que solo puede acceder un usuario autenticado /autenticado/ruta
+	autenticado := r.Group("/autenticado")
+	autenticado.Use(middlewares.ValidarUsuario())
 	{
-		validate.GET("/libros", controllers.BuscarLibroJWT)
-		validate.POST("/libros", controllers.CrearLibroJWT)
-		validate.POST("/libros/categoria_nueva", controllers.AgregarCategoriaJWT)
+		autenticado.GET("/inicio", controllers.GetLibros)
+		autenticado.GET("/logout", controllers.Logout)
+		autenticado.POST("/inicio/libros/comprar/:id", controllers.ComprarLibro)
+		autenticado.POST("/inicio/libros/prestamo/:id", controllers.PrestamoLibro)
+		autenticado.GET("/inicio/libros/adquiridos", controllers.GetLibrosAdquiridos)
+		isAdmin := autenticado.Group("/admin")
+		isAdmin.Use(middlewares.ValidarAdmin())
+		isAdmin.GET("/libros", controllers.GetLibrosAdmin)
+		isAdmin.GET("/libros/agregar", func(ctx *gin.Context) {
+			ctx.HTML(200, "crear_libro.html", nil)
+		})
+		isAdmin.POST("/libros/agregar", controllers.AgregarLibro)
+		isAdmin.GET("/libros/editar/:id", controllers.MostrarEditarLibro)
+		isAdmin.POST("/libros/editar/:id", controllers.ActualizarLibro)
+		isAdmin.POST("/libros/eliminar/:id", controllers.EliminarLibro)
+		isAdmin.GET("/logout", controllers.Logout)
 	}
-	router.Run()
+	r.Run()
 }
